0072682d03d56b4521b69a3db0218120c7c6cdd5
SebastianG302 <johan.guerrero@pi.edu.co>
2022-10-06 00:50:46 -0500

    profile description added

---
+++ src/app/shared/profile-description/profile-description.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-description',
+  templateUrl: './profile-description.component.html',
+  styleUrls: ['./profile-description.component.css']
+})
+export class ProfileDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileDescriptionComponent } from './profile-description/profile-description.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    ProfileDescriptionComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    ProfileDescriptionComponent
   ]
 })
 export class SharedModule { }
